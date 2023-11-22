4473f2fe32bdacf4baae8f5325d7616241fb2dd4
Juan Carlos Del Rio <jcdre@hotmail.com>
2023-11-21 19:17:38 -0500

    project06 day update

---
+++ projects/06-shopping-cart/src/App.jsx
@@ -0,0 +1,26 @@
+import { products as initialProducts } from './mocks/products.json'
+import { Products } from './components/Products'
+import { useState } from 'react'
+import { Header } from './components/Header'
+import { Footer } from './components/Footer'
+import { IS_DEVELOPMENT } from './config.js'
+import { useFilters } from './hooks/useFilters.js'
+import { Cart } from './components/Cart.jsx'
+import { CartProvider } from './context/cart.jsx'
+
+function App() {
+  const { filterProducts } = useFilters()
+  const [products] = useState(initialProducts)
+
+  const filteredProducts = filterProducts(products)
+  return (
+    <CartProvider>
+      <Header />
+      <Cart />
+      <Products products={filteredProducts} />
+      {IS_DEVELOPMENT && <Footer />}
+    </CartProvider>
+  )
+}
+
+export default App
---
+++ projects/06-shopping-cart/src/components/Products.jsx
@@ -0,0 +1,26 @@
+import './Products.css'
+import { AddToCartIcon } from './Icons'
+import { useCart } from '../hooks/useCart'
+
+export function Products({ products }) {
+  const { addToCart } = useCart()
+  return (
+    <main className='products'>
+      <ul>
+        {products.slice(0, 10).map((product) => (
+          <li key={product.id}>
+            <img src={product.thumbnail} alt={product.title} />
+            <div>
+              <strong>{product.title}</strong> - ${product.price}
+            </div>
+            <div>
+              <button onClick={() => addToCart(product)}>
+                <AddToCartIcon />
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
+}
